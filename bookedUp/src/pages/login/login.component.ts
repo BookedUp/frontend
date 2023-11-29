3ce803d80d24075f00c0747b210732021c8f0142
0101dusica <trbovicdusica@gmail.com>
2023-11-29 18:50:51 +0100

    login page done

---
+++ bookedUp/src/pages/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css', '../../styles.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    var frameContainer = document.getElementById("frameContainer");
+    if (frameContainer) {
+      frameContainer.addEventListener("click", () => {
+        const guestRole = document.getElementById("guestRole");
+
+        if (guestRole && guestRole.style.display === 'none') {
+          this.router.navigate(['/register-owner-step2']);
+        } else {
+          this.router.navigate(['/register-guest-step2']);
+        }
+      });
+    }
+
+    var alreadyHaveAn = document.getElementById("dontHaveAn");
+    if (alreadyHaveAn) {
+      alreadyHaveAn.addEventListener("click", () => {
+        this.router.navigate(['/register']);
+      });
+    }
+  }
+
+}
