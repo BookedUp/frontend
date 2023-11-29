953f486a5993287b8b68920dd42cd878af2a6f99
0101dusica <trbovicdusica@gmail.com>
2023-11-29 23:40:38 +0100

    Links Changed

--- bookedUp/src/pages/login/login.component.ts
+++ bookedUp/src/pages/login/login.component.ts
@@ -13,23 +13,17 @@ export class LoginComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit() {
-    var frameContainer = document.getElementById("frameContainer");
-    if (frameContainer) {
-      frameContainer.addEventListener("click", () => {
-        const guestRole = document.getElementById("guestRole");
-
-        if (guestRole && guestRole.style.display === 'none') {
-          this.router.navigate(['/register-owner-step2']);
-        } else {
-          this.router.navigate(['/register-guest-step2']);
-        }
-      });
-    }
-
     var alreadyHaveAn = document.getElementById("dontHaveAn");
     if (alreadyHaveAn) {
       alreadyHaveAn.addEventListener("click", () => {
         this.router.navigate(['/register']);
+      });
+    }
+
+    var frameContainer = document.getElementById("frameContainer");
+    if (frameContainer) {
+      frameContainer.addEventListener("click", () => {
+        this.router.navigate(['/user-main-page']);
       });
     }
 
