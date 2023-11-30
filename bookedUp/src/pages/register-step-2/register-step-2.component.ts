c58e7f08e24fe23a0adf34c44326723d9d90792a
Vesna Vasic <vasic.vesna.06@gmail.com>
2023-11-30 14:46:59 +0100

    Enable text input on Register, Register-step-2 and forgot passwrod

--- bookedUp/src/pages/register-step-2/register-step-2.component.ts
+++ bookedUp/src/pages/register-step-2/register-step-2.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-step-2.component.css', '../../styles.css']
 })
 export class RegisterStep2Component {
+  isPasswordVisible1: boolean = false;
+  isPasswordVisible2: boolean = false;
 
+
+  togglePasswordVisibility1() {
+    this.isPasswordVisible1 = !this.isPasswordVisible1;
+  }
+  togglePasswordVisibility2() {
+    this.isPasswordVisible2 = !this.isPasswordVisible2;
+  }
 }
