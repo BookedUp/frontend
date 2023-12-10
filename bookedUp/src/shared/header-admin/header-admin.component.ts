8acbcabbb1f8dfe6c099dcc02ae46e8eb2edfeaa
0101dusica <trbovicdusica@gmail.com>
2023-12-10 13:27:55 +0100

    popuplogic changed

--- bookedUp/src/shared/header-admin/header-admin.component.ts
+++ bookedUp/src/shared/header-admin/header-admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,59 +6,31 @@ import { Router } from '@angular/router';
   templateUrl: './header-admin.component.html',
   styleUrls: ['./header-admin.component.css', '../../styles.css']
 })
-export class HeaderAdminComponent implements OnInit {
+export class HeaderAdminComponent {
+
+  isPopupVisible = false;
 
   constructor(private router: Router) {}
 
-  ngOnInit() {
+  onProfilePictureClick(): void {
+    this.isPopupVisible = !this.isPopupVisible;
+  }
 
-    var profilePict = document.getElementById("profile-picture-admin");
-    if (profilePict) {
-      profilePict.addEventListener("click", () => {
-        var popup = document.getElementById("adminDropdownContainer");
-        var popupStyle = popup?.style;
-
-        if (popupStyle) {
-          popupStyle.display = "flex";
-          popupStyle.zIndex = "100";
-          popupStyle.backgroundColor = "rgba(113, 113, 113, 0.3)";
-          popupStyle.alignItems = "center";
-          popupStyle.justifyContent = "center";
-        }
-
-        popup?.setAttribute("closable", "");
-
-        var onClick = (popup && popup.onclick) || function (e: Event) {
-          if (e.target === popup && popup && popup.hasAttribute("closable")) {
-            popup.style.display = "none";
-          }
-        };
-
-        popup?.addEventListener("click", onClick);
-      });
+  onPopupClick(event: Event): void {
+    if (this.isPopupVisible && event.target instanceof HTMLElement && event.target.hasAttribute('closable')) {
+      this.isPopupVisible = false;
     }
+  }
 
+  navigateToManageProfile(): void {
+    this.router.navigate(['/manage-profile'], { queryParams: { role: 'admin' } });
+  }
 
-    var popupframeContainer = document.getElementById("popupframeContainer");
-    if (popupframeContainer) {
-      popupframeContainer.addEventListener("click", () => {
-        this.router.navigate(['/manage-profile']);
-      });
-    }
+  navigateToHome(): void {
+    this.router.navigate(['/'], { queryParams: { role: 'admin' } });
+  }
 
-    var homeText = document.getElementById("homeText");
-    if (homeText) {
-      homeText.addEventListener("click", () => {
-        this.router.navigate(['/user-main-page']);
-      });
-    }
-
-
-    var popupframeContainer4 = document.getElementById("popupframeContainer4");
-    if (popupframeContainer4) {
-      popupframeContainer4.addEventListener("click", () => {
-        this.router.navigate(['/']);
-      });
-    }
+  navigateToDefault(): void {
+    this.router.navigate(['/']);
   }
 }
