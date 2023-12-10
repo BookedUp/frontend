29efc33953345751509aa57b5c9aeda3e4312b8b
0101dusica <trbovicdusica@gmail.com>
2023-12-10 13:27:35 +0100

    popup logic changed

--- bookedUp/src/shared/header-host/header-host.component.ts
+++ bookedUp/src/shared/header-host/header-host.component.ts
@@ -7,86 +7,46 @@ import { Router } from '@angular/router';
   templateUrl: './header-host.component.html',
   styleUrls: ['./header-host.component.css', '../../styles.css']
 })
-export class HeaderHostComponent implements OnInit {
+export class HeaderHostComponent {
+  isPopupVisible = false;
 
   constructor(private router: Router) {}
 
-  ngOnInit() {
+  onProfilePictureClick(): void {
+    this.isPopupVisible = !this.isPopupVisible;
+  }
 
-    var profilePict = document.getElementById("profile-picture-host");
-    if (profilePict) {
-      profilePict.addEventListener("click", () => {
-        var popup = document.getElementById("hostDropdownContainer");
-        var popupStyle = popup?.style;
-
-        if (popupStyle) {
-          popupStyle.display = "flex";
-          popupStyle.zIndex = "100";
-          popupStyle.backgroundColor = "rgba(113, 113, 113, 0.3)";
-          popupStyle.alignItems = "center";
-          popupStyle.justifyContent = "center";
-        }
-
-        popup?.setAttribute("closable", "");
-
-        var onClick = (popup && popup.onclick) || function (e: Event) {
-          if (e.target === popup && popup && popup.hasAttribute("closable")) {
-            popup.style.display = "none";
-          }
-        };
-
-        popup?.addEventListener("click", onClick);
-      });
+  onPopupClick(event: Event): void {
+    if (this.isPopupVisible && event.target instanceof HTMLElement && event.target.hasAttribute('closable')) {
+      this.isPopupVisible = false;
     }
+  }
 
+  navigateToManageAccount(): void {
+    this.router.navigate(['/manage-profile'], { queryParams: { role: 'host' } });
+  }
 
-    var manageAcc = document.getElementById("manageAcc");
-    if (manageAcc) {
-      manageAcc.addEventListener("click", () => {
-        this.router.navigate(['/']);
-      });
-    }
+  navigateToMyAmenities(): void {
+    this.router.navigate(['/'], { queryParams: { role: 'host' } });
+  }
 
-    var myAmenities = document.getElementById("myAmenities");
-    if (myAmenities) {
-      myAmenities.addEventListener("click", () => {
-        this.router.navigate(['/']);
-      });
-    }
+  navigateToReservations(): void {
+    this.router.navigate(['/'], { queryParams: { role: 'host' } });
+  }
 
-    var favourties = document.getElementById("reservations");
-    if (favourties) {
-      favourties.addEventListener("click", () => {
-        this.router.navigate(['/']);
-      });
-    }
+  navigateToComments(): void {
+    this.router.navigate(['/'], { queryParams: { role: 'host' } });
+  }
 
-    var comments = document.getElementById("comments");
-    if (comments) {
-      comments.addEventListener("click", () => {
-        this.router.navigate(['/']);
-      });
-    }
+  navigateToReports(): void {
+    this.router.navigate(['/'], { queryParams: { role: 'host' } });
+  }
 
-    var reports = document.getElementById("reports");
-    if (reports) {
-      reports.addEventListener("click", () => {
-        this.router.navigate(['/']);
-      });
-    }
+  navigateToHome(): void {
+    this.router.navigate(['/'], { queryParams: { role: 'host' } });
+  }
 
-    var popupframeContainer4 = document.getElementById("signOut");
-    if (popupframeContainer4) {
-      popupframeContainer4.addEventListener("click", () => {
-        this.router.navigate(['/']);
-      });
-    }
-
-    var homeText = document.getElementById("homeText");
-    if (homeText) {
-      homeText.addEventListener("click", () => {
-        this.router.navigate(['/']);
-      });
-    }
+  navigateToSignOut(): void {
+    this.router.navigate(['/']);
   }
 }
