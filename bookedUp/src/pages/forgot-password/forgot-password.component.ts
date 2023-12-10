fc8edd3021a16676b9f0e60509cb28786be55655
0101dusica <trbovicdusica@gmail.com>
2023-12-10 13:56:16 +0100

    checking email before continue

--- bookedUp/src/pages/forgot-password/forgot-password.component.ts
+++ bookedUp/src/pages/forgot-password/forgot-password.component.ts
@@ -11,13 +11,22 @@ export class ForgotPasswordComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit() {
-
     var frameContainer = document.getElementById("resetLink");
     if (frameContainer) {
       frameContainer.addEventListener("click", () => {
-        this.router.navigate(['/check-inbox']);
+        var emailInput = document.getElementById("emailInput") as HTMLInputElement;
+
+        if (this.isEmailValid(emailInput.value)) {
+          this.router.navigate(['/check-inbox', { email: emailInput.value }]);
+        } else {
+          alert("Invalid email address");
+        }
       });
     }
+  }
 
+  isEmailValid(email: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
   }
 }
