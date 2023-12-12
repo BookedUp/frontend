b9788d41c8586d94cdbd30b6e75c6af301937fd6
0101dusica <trbovicdusica@gmail.com>
2023-12-12 10:32:36 +0100

    dates chnaged

--- bookedUp/src/pages/index/index.component.ts
+++ bookedUp/src/pages/index/index.component.ts
@@ -10,10 +10,6 @@ import { AccommodationService } from '../../app/core/services/accommodation.serv
 
 export class IndexComponent implements OnInit {
 
-  location: string = "";
-  startDate = new Date();
-  endDate = new Date();
-  guestNumber :number = 0;
 
   constructor(private router: Router, private route: ActivatedRoute, private accommodationService: AccommodationService) { }
 
@@ -22,26 +18,26 @@ export class IndexComponent implements OnInit {
         if (searchButton) {
             searchButton.addEventListener("click", () => {
                 const roleParam = this.route.snapshot.queryParams['role'];
+                const location = document.getElementById("locationTxt") as HTMLInputElement;
+                const guestNumber = document.getElementById("guestNumberTxt") as HTMLInputElement;
+                
+                const fromDateInput = document.getElementById("fromDate") as HTMLInputElement;
+                const selectedFromDate = fromDateInput.value;
+          
+                const toDateInput = document.getElementById("toDate") as HTMLInputElement;
+                const selectedToDate = toDateInput.value;
 
 
-                this.location = (document.getElementById("locationTxt") as HTMLInputElement)?.value || "";
-                this.startDate = new Date((document.getElementById("startDateTxt") as HTMLInputElement)?.value || '');
-                this.endDate = new Date((document.getElementById("endDateTxt") as HTMLInputElement)?.value || '');
-                this.guestNumber = parseInt((document.getElementById("guestNumberTxt") as HTMLInputElement)?.value || '0', 10);
-
-
-
-                this.accommodationService.searchAccommodations(this.location, this.guestNumber, this.startDate, this.endDate)
+                this.accommodationService.searchAccommodations(location, selectedFromDate, selectedToDate, guestNumber)
                     .subscribe((results) => {
-                        // Rad sa rezultatima pretrage, na primer, redirekcija na drugu rutu
                         if (roleParam === 'admin') {
-                            this.router.navigate(['/search'], { queryParams: { role: 'admin', location: this.location } });
+                            this.router.navigate(['/search'], { queryParams: { role: 'admin', location: location } });
                         } else if (roleParam === 'host') {
-                            this.router.navigate(['/search'], { queryParams: { role: 'host', location: this.location } });
+                            this.router.navigate(['/search'], { queryParams: { role: 'host', location: location } });
                         } else if (roleParam === 'guest') {
-                            this.router.navigate(['/search'], { queryParams: { role: 'guest', location: this.location } });
+                            this.router.navigate(['/search'], { queryParams: { role: 'guest', location: location } });
                         } else {
-                            this.router.navigate(['/search'], { queryParams: { location: this.location } });
+                            this.router.navigate(['/search'], { queryParams: { location: location} });
                         }
                     });
             });
