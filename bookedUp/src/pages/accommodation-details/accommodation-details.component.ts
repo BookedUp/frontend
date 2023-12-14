948bb98e7e660d4588052c25eecfe8d36850d2c3
0101dusica <trbovicdusica@gmail.com>
2023-12-14 23:51:03 +0100

    acc details responsive

--- bookedUp/src/pages/accommodation-details/accommodation-details.component.ts
+++ bookedUp/src/pages/accommodation-details/accommodation-details.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-accommodation-details',
   templateUrl: './accommodation-details.component.html',
-  styleUrls: ['./accommodation-details.component.css', '../../styles.css']
+  styleUrls: ['./accommodation-details.component.css'],
 })
-export class AccommodationDetailsComponent{
+export class AccommodationDetailsComponent implements OnInit {
+  pictureUrls: string[] = [
+    'https://www.essexapartmenthomes.com/-/media/Project/EssexPropertyTrust/Sites/EssexApartmentHomes/Blog/2021/2021-01-12-Studio-vs-One-Bedroom-Apartment-How-They-Measure-Up/Studio-vs-One-Bedroom-Apartment-How-They-Measure-Up-1.jpg',
+    'https://www.thespruce.com/thmb/cPaZ1eA5WqKLYH4kSry7wrMYsDM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/EleanorEmail-847750221ae1499c972c977f279d2d5e.png',
+    'https://assets.nestiostatic.com/building_medias/full/d1c6422ba404e8cc5f87bdf76e0e96b7.jpg',
+    'https://images1.forrent.com/i2/LetRViB02weV_zpN2v0D_VvhdCiiYoo0lNQuCtRUKBo/112/image.jpg',
+    'https://i.pinimg.com/736x/f9/e2/53/f9e253dc1d18b53cacc560896cad8789.jpg',
+  ];
 
+  selectedClass: string = 'bar-text';
+  currentIndex: number = 0;
+
+  constructor( private router: Router ) {}
+
+  ngOnInit(): void {}
+
+  nextImage() {
+    if (this.currentIndex < this.pictureUrls.length - 1) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0;
+    }
+  }
+
+  changeStyle(className: string): void {
+    this.selectedClass = className;
+    if (className === 'bar-text-review') {
+      this.router.navigate(['/accommodation-details'], { queryParams: { filter: 'review' } });
+    } else {
+      this.router.navigate(['/accommodation-details'], { queryParams: { filter: 'overview' } });
+    }
+  }
   
+  generateStars(rating: number): string[] {
+    const stars: string[] = [];
+    for (let i = 1; i <= 5; i++) {
+      if (i <= rating) {
+        stars.push('★');
+      } else if (i - 0.5 === rating) {
+        stars.push('✯');
+      } else {
+        stars.push('☆');
+      }
+    }
+    return stars;
+  }
+
+  roundHalf(value: number): number {
+    return Math.round(value * 2) / 2;
+  }
 }
