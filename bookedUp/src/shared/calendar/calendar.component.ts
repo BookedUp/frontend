77227e01a28b254db73c24df3b68bad907299f7b
0101dusica <trbovicdusica@gmail.com>
2023-12-16 16:42:12 +0100

    path chnaged

--- bookedUp/src/shared/calendar/calendar.component.ts
+++ bookedUp/src/shared/calendar/calendar.component.ts
@@ -5,8 +5,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css'],
+  styleUrls: ['./calendar.component.css', '../../styles.css'],
 })
+
 export class CalendarComponent {
   defaultPrice: number = 10;
   customPrices: { [date: string]: number } = {
