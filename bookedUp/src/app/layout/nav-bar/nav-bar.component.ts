efc92e97dab839eea2ca89f1cfb40f0fa7c13f57
0101dusica <trbovicdusica@gmail.com>
2023-12-16 19:13:50 +0100

    nav bar routing

--- bookedUp/src/app/layout/nav-bar/nav-bar.component.ts
+++ bookedUp/src/app/layout/nav-bar/nav-bar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit{
+  role: string = '' ;
+  constructor(private authService: AuthService) {
+  }
 
+  ngOnInit(): void {
+    this.authService.userState.subscribe((result) => {
+      this.role = result;
+    })
+  }
 }
