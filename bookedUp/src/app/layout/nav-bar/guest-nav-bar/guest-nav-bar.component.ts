c6d5dfe7ee325e743b57c26a6204003cdab820b3
0101dusica <trbovicdusica@gmail.com>
2023-12-16 18:51:21 +0100

    guest nav bar added

--- bookedUp/src/app/layout/nav-bar/guest-nav-bar/guest-nav-bar.component.ts
+++ bookedUp/src/app/layout/nav-bar/guest-nav-bar/guest-nav-bar.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-guest-nav-bar',
   templateUrl: './guest-nav-bar.component.html',
-  styleUrls: ['./guest-nav-bar.component.css']
+  styleUrls: ['./guest-nav-bar.component.css', '../../../../styles.css']
 })
 export class GuestNavBarComponent {
+  isPopupVisible = false;
 
+  constructor(private router: Router) {}
+
+  onProfilePictureClick(): void {
+    this.isPopupVisible = !this.isPopupVisible;
+  }
+
+  onPopupClick(event: Event): void {
+    if (this.isPopupVisible && event.target instanceof HTMLElement && event.target.hasAttribute('closable')) {
+      this.isPopupVisible = false;
+    }
+  }
+
+  navigateToManageAccount(): void {
+    this.router.navigate(['/manage-profile'], { queryParams: { role: 'guest' } });
+  }
+
+  navigateToMyReservations(): void {
+    this.router.navigate(['/'], { queryParams: { role: 'guest' } });
+  }
+
+  navigateToFavourites(): void {
+    this.router.navigate(['/'], { queryParams: { role: 'guest' } });
+  }
+
+  navigateToReviews(): void {
+    this.router.navigate(['/'], { queryParams: { role: 'guest' } });
+  }
+
+  navigateToHome(): void {
+    this.router.navigate(['/'], { queryParams: { role: 'guest' } });
+  }
+
+  navigateToSignOut(): void {
+    this.router.navigate(['/']);
+  }
 }
