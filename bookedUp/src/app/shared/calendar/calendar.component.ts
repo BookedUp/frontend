108a440a645873472f519324afae465628a9cf41
0101dusica <trbovicdusica@gmail.com>
2023-12-17 00:07:31 +0100

    calendar component place chnaged

--- bookedUp/src/app/shared/calendar/calendar.component.ts
+++ bookedUp/src/app/shared/calendar/calendar.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css', '../../styles.css'],
+  styleUrls: ['./calendar.component.css', '../../../styles.css'],
 })
 
 export class CalendarComponent {
