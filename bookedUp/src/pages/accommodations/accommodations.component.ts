ea5cd1b2bbe0d47c3dd4d1c7d3b3f62fb3126cad
0101dusica <trbovicdusica@gmail.com>
2023-12-16 12:40:30 +0100

    new accommodation links added

--- bookedUp/src/pages/accommodations/accommodations.component.ts
+++ bookedUp/src/pages/accommodations/accommodations.component.ts
@@ -20,13 +20,6 @@ export class AccommodationsComponent implements OnInit {
       this.filter = params['filter'] || 'active';
       this.loadAccommodations();
     });
-
-    var newAcc = document.getElementById("new-acc");
-      if (newAcc) {
-        newAcc.addEventListener("click", () => {
-          this.router.navigate(['/add-new-accommodation'], );
-      });
-    }
   }
 
   navigateToDetails(id: number) {
@@ -42,6 +35,10 @@ export class AccommodationsComponent implements OnInit {
             this.accommodations = this.accommodationService.getAllRejectedByHostId(2);
 
         }
+    }
+
+    navigateToCreateAccommodation() {
+        this.router.navigate(['/add-new-accommodation']);
     }
 
     changeStyle(className: string): void {
