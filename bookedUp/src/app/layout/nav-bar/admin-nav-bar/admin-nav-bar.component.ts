a28f11cfc3209cb0ef06cb58f4d41ffdb452d93e
0101dusica <trbovicdusica@gmail.com>
2023-12-16 18:51:29 +0100

    admin nav bar added

--- bookedUp/src/app/layout/nav-bar/admin-nav-bar/admin-nav-bar.component.ts
+++ bookedUp/src/app/layout/nav-bar/admin-nav-bar/admin-nav-bar.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-nav-bar',
   templateUrl: './admin-nav-bar.component.html',
-  styleUrls: ['./admin-nav-bar.component.css']
+  styleUrls: ['./admin-nav-bar.component.css', '../../../../styles.css']
 })
 export class AdminNavBarComponent {
+  isPopupVisible = false;
 
+  constructor(private router: Router) {}
+
+  onProfilePictureClick(): void {
+    this.isPopupVisible = !this.isPopupVisible;
+  }
+
+  onPopupClick(event: Event): void {
+    if (this.isPopupVisible && event.target instanceof HTMLElement && event.target.hasAttribute('closable')) {
+      this.isPopupVisible = false;
+    }
+  }
+
+  navigateToManageProfile(): void {
+    this.router.navigate(['/manage-profile'], { queryParams: { role: 'admin' } });
+  }
+
+  navigateToAmenities(): void {
+    this.router.navigate(['/accommodation-requests'], { queryParams: { filter: 'all' } });
+  }
+
+  navigateToHome(): void {
+    this.router.navigate(['/'], { queryParams: { role: 'admin' } });
+  }
+
+  navigateToDefault(): void {
+    this.router.navigate(['/']);
+  }
 }
