f839d646c0e93373823ea4164ac69c75fa48b86d
0101dusica <trbovicdusica@gmail.com>
2023-12-17 02:01:42 +0100

    routings chnaged

--- bookedUp/src/app/layout/nav-bar/nav-bar.component.ts
+++ bookedUp/src/app/layout/nav-bar/nav-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/auth/auth.service';
+import { AuthService } from 'src/app/infrastructure/auth/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
