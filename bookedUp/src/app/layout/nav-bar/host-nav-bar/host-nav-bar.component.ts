32e0cd1c7967e327bf8206cb7b9b1e2e6cb3ce95
0101dusica <trbovicdusica@gmail.com>
2023-12-17 02:01:24 +0100

    routings chnaged

--- bookedUp/src/app/layout/nav-bar/host-nav-bar/host-nav-bar.component.ts
+++ bookedUp/src/app/layout/nav-bar/host-nav-bar/host-nav-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/auth/auth.service';
+import { AuthService } from 'src/app/infrastructure/auth/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -27,30 +27,6 @@ export class HostNavBarComponent implements OnInit{
     if (this.isPopupVisible && event.target instanceof HTMLElement && event.target.hasAttribute('closable')) {
       this.isPopupVisible = false;
     }
-  }
-
-  navigateToManageAccount(): void {
-    this.router.navigate(['/manage-profile'], { queryParams: { role: 'host' } });
-  }
-
-  navigateToMyAmenities(): void {
-    this.router.navigate(['/accommodations'], { queryParams: { role: 'host' } });
-  }
-
-  navigateToReservations(): void {
-    this.router.navigate(['/reservation-requests'], { queryParams: { filter: 'all' },  });
-  }
-
-  navigateToComments(): void {
-    this.router.navigate(['/'], { queryParams: { role: 'host' } });
-  }
-
-  navigateToReports(): void {
-    this.router.navigate(['/'], { queryParams: { role: 'host' } });
-  }
-
-  navigateToHome(): void {
-    this.router.navigate(['/'], { queryParams: { role: 'host' } });
   }
 
   logOut(): void {
