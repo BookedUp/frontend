e6b8b8565e24d8a33bc82fdc1607caa3b8d46373
0101dusica <trbovicdusica@gmail.com>
2023-12-18 13:23:52 +0100

    updated image chnaged

--- bookedUp/src/app/user/manage-profile/manage-profile.component.ts
+++ bookedUp/src/app/user/manage-profile/manage-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component , OnInit } from '@angular/core';
+import { Component , OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import {User} from "../model/user.model";
 import {UserService} from "../user.service";
@@ -6,79 +6,48 @@ import {UserService} from "../user.service";
 @Component({
   selector: 'app-manage-profile',
   templateUrl: './manage-profile.component.html',
-  styleUrls: ['./manage-profile.component.css', '../../../styles.css']
+  styleUrls: ['./manage-profile.component.css']
 })
 export class ManageProfileComponent implements OnInit {
 
   isPasswordVisible: boolean = false;
   loggedUser!: User;
+  displayedImageUrl: string | null = null;
+  @ViewChild('fileInput') fileInput!: ElementRef;
+
   constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
-    this.userService.getUser(2).subscribe(
+      this.userService.getUser(2).subscribe(
         (user: User) => {
           this.loggedUser = user;
         },
         (error) => {
           console.error('Error loading user:', error);
-          // Handle error as needed
         }
     );
-
-
-    const frameContainer10 = document.getElementById("delete-button");
-
-    if (frameContainer10) {
-      frameContainer10.addEventListener("click", function () {
-        const popup = document.getElementById("deleteAccountNotification");
-
-        if (!popup) return;
-
-        const popupStyle = popup.style;
-
-        if (popupStyle) {
-          popupStyle.display = "flex";
-          popupStyle.zIndex = "100";
-          popupStyle.backgroundColor = "rgba(113, 113, 113, 0.3)";
-          popupStyle.alignItems = "center";
-          popupStyle.justifyContent = "center";
-        }
-
-        popup.setAttribute("closable", "");
-
-        const onClick = (popup && popup.onclick) || function (e: Event) {
-          if (popup && e.target === popup && popup.hasAttribute("closable")) {
-            popup.style.display = "none";
-          }
-        };
-
-        popup.addEventListener("click", onClick);
-      });
-    }
   }
 
+  ngAfterViewInit() {
+    this.fileInput.nativeElement.addEventListener('change', (event: any) => {
+      
+      const file = event.target.files[0];
+      console.log('Nova slika dodata', event.target.files[0]);
+      if (file) {
+        const imageUrl = URL.createObjectURL(file);
+        this.displayedImageUrl = imageUrl;
+      }
+    });
+  }
+  handleFileButtonClick() {
+    
+  console.log('File button clicked');
+  this.fileInput.nativeElement.click();
+  console.log('After triggering click');
+  }
   togglePasswordVisibility() {
     this.isPasswordVisible = !this.isPasswordVisible;
   }
-  // Assuming you have a function to handle file upload in your component
-  // onFileUpload(event: any): void {
-  //     const file = event.target.files[0];
-    
-  //     // Check if loggedUser is defined and has a profilePicture property
-  //     if (this.loggedUser && this.loggedUser.profilePicture) {
-  //       // Perform the logic to upload the file and get the new URL
-  //       // Replace the following line with your actual upload logic
-  //       const newImageUrl = this.uploadImage(file);
-    
-  //       // Update the loggedUser with the new URL
-  //       if (this.loggedUser.profilePicture) {
-  //         this.loggedUser.profilePicture.url = newImageUrl;
-  //       } else {
-  //         // If profilePicture is undefined, create a new object
-  //         this.loggedUser.profilePicture = { url: newImageUrl };
-  //       }
-  //     }
-  //   }
   
 
 }
