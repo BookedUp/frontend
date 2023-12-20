90f5cd796168e5ac34a5d796ee9212b965b2ce63
0101dusica <trbovicdusica@gmail.com>
2023-12-20 11:34:05 +0100

    connect data from back

--- bookedUp/src/app/accommodation/update-accommodation/update-accommodation.component.ts
+++ bookedUp/src/app/accommodation/update-accommodation/update-accommodation.component.ts
@@ -13,6 +13,8 @@ import {Accommodation} from "../model/accommodation.model";
 import {AccommodationStatus} from "../model/enum/accommodationStatus.enum";
 import Swal from "sweetalert2";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { Observable, map } from 'rxjs';
+import { Photo } from 'src/app/shared/model/photo.model';
 
 @Component({
   selector: 'app-update-accommodation',
@@ -20,13 +22,25 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
   styleUrls: ['./update-accommodation.component.css']
 })
 export class UpdateAccommodationComponent implements OnInit {
-@ViewChild('calendarRef') calendarComponent: CalendarComponent | undefined;
+  @ViewChild('calendarRef') calendarComponent: CalendarComponent | undefined;
   amenitiesList : string[] = [];
   accTypeList : string[] = [];
   accTypeChecked: { [key: string]: boolean } = {};
   perNightChecked: boolean = true;
   perGuestChecked: boolean = false;
-
+  defaultPrice: number = 0;
+  minimumPrice: number | undefined;
+  maximumPrice: number | undefined;
+  description: string | undefined;
+  name: string | undefined;
+  addressStreet: string | undefined;
+  city: string | undefined;
+  postalCode: string | undefined;
+  country: string | undefined;
+  acceptReservations: boolean = false;
+  isInputReadOnly: boolean = false;
+  pictureUrls: string[] = [];
+  currentIndex: number = 0;
 
 
   customPricesInput: { [date: string]: number } = { };
@@ -43,6 +57,7 @@ export class UpdateAccommodationComponent implements OnInit {
   availability: DateRange[] = [];
 
   //
+  accommodation: Observable<Accommodation> = new Observable<Accommodation>();
   selectedAccommodation!: Accommodation;
   selectedAccommodationId: number = 0;
 
@@ -75,13 +90,23 @@ export class UpdateAccommodationComponent implements OnInit {
       this.accTypeList.push(this.transformEnumToDisplayFormat(type));
     }
 
+    
+
     this.route.params.subscribe((params) => {
       this.selectedAccommodationId = params['id'];
 
+      this.accommodation = this.accommodationService.getAccommodationById(this.selectedAccommodationId);
       this.accommodationService.getAccommodationById(this.selectedAccommodationId).subscribe(
           (acc: Accommodation) => {
             this.selectedAccommodation = acc;
 
+            if(acc.priceType == PriceType.PerGuest){
+              this.perGuestChecked = true;
+              this.perNightChecked = false;
+            }else{
+              this.perNightChecked = true;
+              this.perGuestChecked = false;
+            }
             this.updateForm!.setValue({
               name: acc.name,
               streetAndNumber: acc.address.streetAndNumber,
@@ -89,8 +114,8 @@ export class UpdateAccommodationComponent implements OnInit {
               postalCode: acc.address.postalCode,
               country: acc.address.country,
               defaultPrice: acc.price,
-              perNightChecked: [true],
-              perGuestChecked: [false],
+              perNightChecked: this.perNightChecked,
+              perGuestChecked: this.perGuestChecked,
               description: acc.description,
               minimumGuest: acc.minGuests,
               maximumGuest: acc.maxGuests,
@@ -110,8 +135,13 @@ export class UpdateAccommodationComponent implements OnInit {
           }
       );
     });
+
+    this.getUrls().subscribe((urls) => {
+      this.pictureUrls = urls;
+    });
   }
 
+  
 
   handlePerNightChange() {
     if (this.perNightChecked) {
@@ -144,84 +174,140 @@ export class UpdateAccommodationComponent implements OnInit {
 
 
   applyCustomPrice(): void {
-    const selectedRange = this.calendarComponent?.getSelectedRange();
+    if(this.isInputReadOnly == false){
+      Swal.fire({
+        title: 'Default Price Changed!',
+        text: 'You can no longer change it in this window.',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'OK',
+        cancelButtonText: 'Cancel',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          if(this.defaultPrice != 0){
+            this.isInputReadOnly = true;
+            const selectedRange = this.calendarComponent?.getSelectedRange();
+            if (selectedRange != null && selectedRange.start != null && selectedRange.start !== undefined && selectedRange.end != null && selectedRange.end !== undefined) {
+              
+              const startDate = new Date(selectedRange.start);
+              const endDate = new Date(selectedRange.end);
+      
+              const newPriceChangeStart: PriceChange = {
+                changeDate: startDate,
+                newPrice: this.customPrice,
+              };
+      
+              this.accPriceChange = [...this.accPriceChange, newPriceChangeStart];
+      
+      
+              var lastDate = new Date(selectedRange.end);
+              lastDate.setDate(lastDate.getDate() + 1);
+              const newPriceChange: PriceChange = {
+                changeDate: lastDate,
+                newPrice: this.defaultPrice,
+              };
+        
+              this.accPriceChange = [...this.accPriceChange, newPriceChange];
+          }
+          }else{
+            alert("You didn't input default price. Please provide a default price before entering a custom one.");
+            return;
+          }
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+          return;
+        }
+      });
+    }else{
+      if(this.defaultPrice != 0){
+        this.isInputReadOnly = true;
+        const selectedRange = this.calendarComponent?.getSelectedRange();
+        if (selectedRange != null && selectedRange.start != null && selectedRange.start !== undefined && selectedRange.end != null && selectedRange.end !== undefined) {
+          
+          const startDate = new Date(selectedRange.start);
+          const endDate = new Date(selectedRange.end);
+  
+          const newPriceChangeStart: PriceChange = {
+            changeDate: startDate,
+            newPrice: this.customPrice,
+          };
+  
+          this.accPriceChange = [...this.accPriceChange, newPriceChangeStart];
+  
+  
+          var lastDate = new Date(selectedRange.end);
+          lastDate.setDate(lastDate.getDate() + 1);
+          const newPriceChange: PriceChange = {
+            changeDate: lastDate,
+            newPrice: this.defaultPrice,
+          };
+    
+          this.accPriceChange = [...this.accPriceChange, newPriceChange];
+      }
+      }else{
+        alert("You didn't input default price. Please provide a default price before entering a custom one.");
+        return;
+      }
+    }
+    
 
-    if (selectedRange != null && selectedRange.start != null && selectedRange.start !== undefined) {
-    const newPriceChange: PriceChange = {
-      changeDate: new Date(selectedRange.start),
-      newPrice: this.customPrice,
-    };
+    
+  }
 
-    // Dodajte novi PriceChange u customPricesInput
-    this.accPriceChange.push(newPriceChange);
 
-    console.log(this.accPriceChange);
+
+addDateRange() {
+  const selectedDates = this.calendarComponent?.getSelectedRange();
+
+  if (selectedDates?.start !== null && selectedDates?.end !== null && selectedDates?.hasAlreadyPicked) {
+    this.addedDates.push({ start: selectedDates.start, end: selectedDates.end });
+    this.addedDates = this.mergeOverlappingDateRanges(this.addedDates);
+
+    if (this.calendarComponent?.selectedRange) {
+      this.calendarComponent.selectedRange.start = null;
+      this.calendarComponent.selectedRange.end = null;
+    }
+
     this.calendarComponent?.generateCalendar();
+  } else {
+    alert('Please select valid date range!');
   }
 }
 
-
-
-  addDateRange() {
-    const selectedDates = this.calendarComponent?.getSelectedRange();
-
-    if (selectedDates?.start !== null && selectedDates?.end !== null && selectedDates?.hasAlreadyPicked) {
-      this.addedDates.push({ start: selectedDates.start, end: selectedDates.end });
-
-      console.log(this.addedDates);
-
-      this.addedDates = this.mergeOverlappingDateRanges(this.addedDates);
-
-      if (this.calendarComponent?.selectedRange) {
-        this.calendarComponent.selectedRange.start = null;
-        this.calendarComponent.selectedRange.end = null;
-      }
-
-      this.calendarComponent?.generateCalendar();
-    } else {
-      alert('Molimo vas da odaberete validan datumski opseg.');
-    }
-  }
-
-
 private mergeOverlappingDateRanges(dateRanges: { start: string, end: string }[]): { start: string, end: string }[] {
-    dateRanges.sort((a, b) => new Date(a.start).getTime() - new Date(b.start).getTime());
-    const mergedRanges: { start: string, end: string }[] = [];
-    let currentRange = dateRanges[0];
+  dateRanges.sort((a, b) => new Date(a.start).getTime() - new Date(b.start).getTime());
+  const mergedRanges: { start: string, end: string }[] = [];
+  let currentRange = dateRanges[0];
+  for (let i = 1; i < dateRanges.length; i++) {
+    const nextRange = dateRanges[i];
 
-    for (let i = 1; i < dateRanges.length; i++) {
-      const nextRange = dateRanges[i];
+    const currentStartDate = new Date(currentRange.start);
+    const currentEndDate = new Date(currentRange.end);
+    const nextStartDate = new Date(nextRange.start);
+    const nextEndDate = new Date(nextRange.end);
 
-      const currentEndDate = new Date(currentRange.end);
-      const nextStartDate = new Date(nextRange.start);
 
-      if (currentEndDate >= nextStartDate || nextStartDate <= currentEndDate) {
-        // Ranges overlap or touch, merge them
-        currentRange.end = nextRange.end;
-      } else if (nextRange.start >= currentRange.start && nextRange.end <= currentRange.end) {
-        // Next range is completely within the current range, skip it
+    if (currentEndDate >= nextStartDate) {
+      if(currentEndDate >= nextEndDate){
         continue;
-      } else {
-        // Ranges don't overlap and next range is not completely within the current range, add the current range to the result
-        mergedRanges.push(currentRange);
-        currentRange = nextRange;
+      }else{
+        currentRange.end = nextRange.end;
       }
-
-      console.log(dateRanges);
-
+    } else {
+      mergedRanges.push(currentRange);
+      currentRange = nextRange;
     }
-
-    // Add the last range
-    mergedRanges.push(currentRange);
-
-    return mergedRanges;
   }
 
+  mergedRanges.push(currentRange);
 
-  deleteDateRange(): void {
-    const selectedDates = this.calendarComponent?.getSelectedRange();
+  return mergedRanges;
+}
 
-    if (selectedDates?.start && selectedDates?.end) {
+
+deleteDateRange(): void {
+  const selectedDates = this.calendarComponent?.getSelectedRange();
+
+  if (selectedDates?.start && selectedDates?.end) {
     const startSelectedDate = new Date(selectedDates.start);
     const endSelectedDate = new Date(selectedDates.end);
 
@@ -229,30 +315,78 @@ private mergeOverlappingDateRanges(dateRanges: { start: string, end: string }[])
       const startDate = new Date(dateRange.start);
       const endDate = new Date(dateRange.end);
 
-      if (
-          (startDate >= startSelectedDate && startDate <= endSelectedDate) ||
-          (endDate >= startSelectedDate && endDate <= endSelectedDate) ||
-          (startDate <= startSelectedDate && endDate >= endSelectedDate)
-      ) {
-        // Datum iz liste se preklapa sa odabranim opsegom, treba ga smanjiti
-        if (startDate < startSelectedDate) {
-          dateRange.end = this.subtractDays(startSelectedDate, 1).toISOString().split('T')[0];
-        }
+      if (endSelectedDate < startDate || startSelectedDate > endDate) {
+        // No overlap, do nothing
+      } else if (startSelectedDate <= startDate && endSelectedDate >= endDate) {
+        // Selected range completely covers the current range, remove it
+        this.addedDates.splice(index, 1);
+      } else if (startSelectedDate <= startDate && endSelectedDate < endDate) {
+        // Overlapping on the left side, adjust start date
+        dateRange.start = endSelectedDate.toISOString().split('T')[0];
+      } else if (startSelectedDate > startDate && endSelectedDate >= endDate) {
+        // Overlapping on the right side, adjust end date
+        dateRange.end = startSelectedDate.toISOString().split('T')[0];
+      } else if (startSelectedDate > startDate && endSelectedDate < endDate) {
+        // Selected range is in the middle, split the current range
+        const newEndDate = endSelectedDate.toISOString().split('T')[0];
+        dateRange.end = startSelectedDate.toISOString().split('T')[0];
 
-        if (endDate > endSelectedDate) {
-          dateRange.start = this.addDays(endSelectedDate, 1).toISOString().split('T')[0];
-        }
+        // Insert a new range for the right side
+        this.addedDates.splice(index + 1, 0, {
+          start: newEndDate,
+          end: endDate.toISOString().split('T')[0]
+        });
       }
     });
 
-    // Ponovno spoji datume nakon brisanja
     this.addedDates = this.mergeOverlappingDateRanges(this.addedDates);
-
-    // Ako želite ažurirati i kalendar, pozovite odgovarajuće metode
-    // this.calendarComponent?.generateCalendar();
+  }
+}
+nextImage() {
+  if (this.currentIndex < this.pictureUrls.length - 1) {
+    this.currentIndex++;
+  } else {
+    this.currentIndex = 0;
   }
 }
 
+getUrls(): Observable<string[]> {
+  return this.accommodation.pipe(
+    map((accommodation: { photos: Photo[]; }) => accommodation?.photos?.map((photo) => photo.url) || [])
+  );
+}
+
+uploadNewImage(): void {
+  const fileInput = document.getElementById('fileInput') as HTMLInputElement;
+  fileInput.click();
+}
+
+handleFileInputChange(event: Event): void {
+  const inputElement = event.target as HTMLInputElement;
+
+  if (inputElement.files && inputElement.files.length > 0) {
+    const file = inputElement.files[0];
+
+    const imageUrl = URL.createObjectURL(file);
+
+    this.pictureUrls.push(imageUrl);
+
+    inputElement.value = '';
+  }
+  console.log("ovo je posle dodavanja ", this.pictureUrls);
+}
+
+deleteImage():void{
+  if (this.currentIndex >= 0 && this.currentIndex < this.pictureUrls.length) {
+    this.pictureUrls.splice(this.currentIndex, 1);
+    if (this.currentIndex >= this.pictureUrls.length) {
+      this.currentIndex = this.pictureUrls.length - 1;
+    }
+  } else {
+    console.error('Invalid currentIndex value');
+  }
+  console.log("ovo je posle brisanja ", this.pictureUrls);
+}
 
 private subtractDays(date: Date, days: number): Date {
     const result = new Date(date);
