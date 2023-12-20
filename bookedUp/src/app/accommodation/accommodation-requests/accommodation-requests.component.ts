634c9a9e58d749623f9dde571e9b6c5dae8196e8
Vesna Vasic <vasic.vesna.06@gmail.com>
2023-12-20 20:04:49 +0100

    Load pictures on accommodation-requests

--- bookedUp/src/app/accommodation/accommodation-requests/accommodation-requests.component.ts
+++ bookedUp/src/app/accommodation/accommodation-requests/accommodation-requests.component.ts
@@ -4,20 +4,26 @@ import { Router, ActivatedRoute} from '@angular/router';
 import { Accommodation } from '../model/accommodation.model';
 import { Observable } from 'rxjs';
 import Swal from "sweetalert2";
+import {PhotoService} from "../../shared/photo/photo.service";
 
 @Component({
   selector: 'app-accommodation-requests',
   templateUrl: './accommodation-requests.component.html',
   styleUrls: ['./accommodation-requests.component.css', '../../../styles.css']
 })
-export class AccommodationRequestsComponent implements OnInit{
+export class AccommodationRequestsComponent implements OnInit {
   accommodations: Observable<Accommodation[]> = new Observable<Accommodation[]>();
   selectedClass: string = 'all-accommodations';
   filter: string = 'all';
 
   priceTypeGuest: string = 'per guest';
   priceTypeNight: string = 'per night';
-  constructor(private accommodationService: AccommodationService, private router: Router, private route: ActivatedRoute) { }
+
+  photoDict: { accId: number, url: string }[] = [];
+  acc: Accommodation[] = [];
+
+  constructor(private accommodationService: AccommodationService, private router: Router, private route: ActivatedRoute, private photoService: PhotoService) {
+  }
 
 
   ngOnInit(): void {
@@ -30,21 +36,33 @@ export class AccommodationRequestsComponent implements OnInit{
   changeStyle(className: string): void {
     this.selectedClass = className;
     if (className === 'changed-accommodations') {
-      this.router.navigate(['/accommodation-requests'], { queryParams: { filter: 'changed' } });
+      this.router.navigate(['/accommodation-requests'], {queryParams: {filter: 'changed'}});
     } else if (className === 'new-accommodations') {
-      this.router.navigate(['/accommodation-requests'], { queryParams: { filter: 'new' } });
+      this.router.navigate(['/accommodation-requests'], {queryParams: {filter: 'new'}});
     } else {
-      this.router.navigate(['/accommodation-requests'], { queryParams: { filter: 'all' } });
+      this.router.navigate(['/accommodation-requests'], {queryParams: {filter: 'all'}});
     }
   }
 
   private loadAccommodations(): void {
     if (this.filter === 'all') {
       this.accommodations = this.accommodationService.getAllModifiedAccommodations();
+      this.accommodationService.getAllModifiedAccommodations().subscribe((results) => {
+        this.acc = results;
+        this.loadPhotos();
+      });
     } else if (this.filter === 'new') {
       this.accommodations = this.accommodationService.getAllCreatedAccommodations();
+      this.accommodationService.getAllCreatedAccommodations().subscribe((results) => {
+        this.acc = results;
+        this.loadPhotos();
+      });
     } else {
       this.accommodations = this.accommodationService.getAllChangedAccommodations();
+      this.accommodationService.getAllChangedAccommodations().subscribe((results) => {
+        this.acc = results;
+        this.loadPhotos();
+      });
     }
   }
 
@@ -62,9 +80,6 @@ export class AccommodationRequestsComponent implements OnInit{
     return stars;
   }
 
-  roundHalf(value: number): number {
-    return Math.round(value * 2) / 2;
-  }
 
   approveAccommodation(id: number): void {
     this.accommodationService.approveAccommodation(id)
@@ -109,5 +124,51 @@ export class AccommodationRequestsComponent implements OnInit{
               });
             }
         );
+  }
+
+
+  loadPhotos() {
+    this.acc.forEach((acc) => {
+      this.photoService.loadPhoto(acc.photos[0]).subscribe(
+          (data) => {
+            this.createImageFromBlob(data).then((url: string) => {
+              if (acc.id) {
+                this.photoDict.push({accId: acc.id, url: url});
+              }
+            }).catch(error => {
+              console.error("Greška prilikom konverzije slike ${imageName}:", error);
+            });
+          },
+          (error) => {
+            console.log("Doslo je do greske pri ucitavanju slike ${imageName}:", error);
+          }
+      );
+    });
+  }
+
+
+  createImageFromBlob(imageBlob: Blob): Promise<string> {
+    const reader = new FileReader();
+
+    return new Promise<string>((resolve, reject) => {
+      reader.onloadend = () => {
+        resolve(reader.result as string);
+      };
+      reader.onerror = reject;
+      reader.readAsDataURL(imageBlob);
+    });
+  }
+
+  getPhotoUrl(accId: number | undefined): string | undefined {
+    const photo = this.photoDict.find((item) => item.accId === accId);
+    return photo ? photo.url : '';
+  }
+
+
+  roundHalf(value: number | undefined): number | undefined {
+    if (value) {
+      return Math.round(value * 2) / 2;
+    }
+    return 0;
   }
 }
