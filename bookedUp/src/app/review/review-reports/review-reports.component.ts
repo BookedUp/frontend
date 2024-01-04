1cfda1bb8d547ad6f2db9c5f0e685da2d46013fb
0101dusica <trbovicdusica@gmail.com>
2024-01-04 23:57:01 +0100

    Review Reports Page Added

---
+++ bookedUp/src/app/review/review-reports/review-reports.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-reports',
+  templateUrl: './review-reports.component.html',
+  styleUrls: ['./review-reports.component.css']
+})
+export class ReviewReportsComponent {
+
+}
