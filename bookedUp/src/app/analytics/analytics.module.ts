72a60a9654241d89f3763610dad7f8f0d868f0db
0101dusica <trbovicdusica@gmail.com>
2024-01-11 17:14:23 +0100

    analytics module added

---
+++ bookedUp/src/app/analytics/analytics.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MaterialModule } from '../infrastructure/material/material.module';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '../shared/shared.module';
+import { LayoutModule } from '../layout/layout.module';
+
+import { AnalyticsComponent } from './analytics/analytics.component';
+import { SingleAccommodationAnalyticsComponent } from './single-accommodation-analytics/single-accommodation-analytics.component';
+import { YearlyAnalyticsComponent } from './yearly-analytics/yearly-analytics.component';
+
+
+
+@NgModule({
+  declarations: [
+    AnalyticsComponent,
+    SingleAccommodationAnalyticsComponent,
+    YearlyAnalyticsComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    SharedModule,
+    LayoutModule,
+  ],
+  exports:[
+    AnalyticsComponent,
+  ]
+})
+export class AnalyticsModule { }
