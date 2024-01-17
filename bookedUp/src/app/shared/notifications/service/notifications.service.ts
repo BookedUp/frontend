3610a7ca9ffbceff7155e0fbd788d44fa4568abb
0101dusica <trbovicdusica@gmail.com>
2024-01-17 16:30:17 +0100

    updated functions from the back

--- bookedUp/src/app/shared/notifications/service/notifications.service.ts
+++ bookedUp/src/app/shared/notifications/service/notifications.service.ts
@@ -3,37 +3,14 @@ import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { Notification } from '../model/notification.model';
 
-import * as Stomp from 'stompjs';
-import * as SockJS from 'sockjs-client';
-
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
 
-  private baseUrl = 'http://localhost:8080';
-  url: string = this.baseUrl + "/api/socket";
-  restUrl: string = this.baseUrl + "/sendMessageRest";
-  
-  private stompClient: Stomp.Client | undefined;
+  private apiUrl = 'http://localhost:8080/api/notifications';
 
   constructor(private http: HttpClient) { }
-
-  sendMessageUsingSocket(notification: Notification) {
-    this.stompClient!.send(this.baseUrl+"/socket-subscriber/send/message", {}, JSON.stringify(notification));
-  }
-
-  post(data: Notification) {
-    return this.http.post<Notification>(this.url, data)
-      .pipe(map((data: Notification) => { return data; }));
-  }
-
-  postRest(data: Notification) {
-    return this.http.post<Notification>(this.restUrl, data)
-      .pipe(map((data: Notification) => { return data; }));
-  }
-
-  private apiUrl = 'http://localhost:8080/api/notifications';
 
   getNotifications(): Observable<Notification[]> {
     return this.http.get<Notification[]>(this.apiUrl);
