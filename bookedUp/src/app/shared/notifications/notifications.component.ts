e1b4b3d52805a811c87a55dafb394de92167d932
0101dusica <trbovicdusica@gmail.com>
2024-01-17 10:19:27 +0100

    enabled settings notifications chnaged

--- bookedUp/src/app/shared/notifications/notifications.component.ts
+++ bookedUp/src/app/shared/notifications/notifications.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { BehaviorSubject, Observable, Subject, map } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, combineLatest, map, switchMap } from 'rxjs';
 import { AuthService } from 'src/app/infrastructure/auth/auth.service';
 import { NotificationsService } from './service/notifications.service';
 import { Notification } from './model/notification.model';
 
 import * as Stomp from 'stompjs';
 import * as SockJS from 'sockjs-client';
+import { User } from 'src/app/user/model/user.model';
+import { UserService } from 'src/app/user/user.service';
+import { Host } from 'src/app/user/model/host.model';
+import { Guest } from 'src/app/user/model/guest.model';
+import { Role } from 'src/app/user/model/role.enum';
+import { GuestService } from 'src/app/user/guest/guest.service';
+import { HostService } from 'src/app/user/host/host.service';
+import { NotificationType } from './model/enum/notificationType.enum';
 
 @Component({
   selector: 'app-notifications',
@@ -17,15 +25,18 @@ export class NotificationsComponent implements OnInit{
 
   filter: string = 'all-text';
   selectedClass: string = 'all-text';
+  isNotificationVisible: boolean = true;
 
   private notificationsSubject: BehaviorSubject<Notification[]> = new BehaviorSubject<Notification[]>([]);
   notifications$: Observable<Notification[]> = this.notificationsSubject.asObservable();
+  host: Host | undefined;
+  guest: Guest | undefined;
 
   serverUrl:string = 'http://localhost:8080/socket';
   private stompClient: Stomp.Client | undefined;
 
 
-  constructor(private authService: AuthService, private router: Router, private route: ActivatedRoute, private notificationService: NotificationsService) {
+  constructor(private authService: AuthService, private guestService: GuestService, private hostService: HostService, private router: Router, private route: ActivatedRoute, private notificationService: NotificationsService) {
   }
 
 
@@ -36,12 +47,24 @@ export class NotificationsComponent implements OnInit{
       this.filter = params['notificationFilter'] || 'all-text';
       this.loadNotifications();
     });
+
+    if(Role.Guest === this.authService.getRole()){
+      this.guestService.getGuestById(this.authService.getUserID())
+      .subscribe((guest: Guest) => {
+        this.guest = guest;
+      });
+    }else if(Role.Host === this.authService.getRole()){
+      this.hostService.getHost(this.authService.getUserID())
+      .subscribe((host: Host) => {
+        this.host = host;
+        this.host.accommodationRatingNotificationEnabled
+      });
+    }
   }
 
   private loadNotifications() : void {
     if (this.filter === 'all-text') {
-      console.log("This is history");
-      this.notifications$ = this.notificationService.getNotificationsByUserId(this.authService.getUserID())
+      this.notifications$ = this.notificationService.getEnabledNotificationsByUserId(this.authService.getUserID())
       .pipe(
         map(notifications => notifications.sort((a, b) => {
           const timestampA = new Date(a.timestamp).getTime();
@@ -50,23 +73,21 @@ export class NotificationsComponent implements OnInit{
         }))
       );
     }else{
-      console.log("This is Web Socket");
       this.initializeWebSocketConnection();
     }
   }
 
   initializeWebSocketConnection():void {
-    console.log("this is url",this.serverUrl);
-   let ws = new SockJS(this.serverUrl);
+    let ws = new SockJS(this.serverUrl);
 
     this.stompClient = Stomp.over(ws);
-     let that = this;
+    let that = this;
     this.stompClient.connect({}, function (frame) {
       console.log('Connected: ' + frame);
       that.openGlobalSocket()
-   }, function (error) {
-      console.log('Error: ' + error);
-   });
+    }, function (error) {
+        console.log('Error: ' + error);
+    });
 
   }
 
@@ -87,6 +108,10 @@ export class NotificationsComponent implements OnInit{
     }
   }
 
+  navigateTo(route: string): void {
+    this.isNotificationVisible = !this.isNotificationVisible;
+    this.router.navigate([route]);
+  }
 
   changeStyle(className: string): void {
     this.selectedClass = className;
--- bookedUp/src/app/shared/notifications/service/notifications.service.ts
+++ bookedUp/src/app/shared/notifications/service/notifications.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { Notification } from '../model/notification.model';
 
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,8 +14,14 @@ export class NotificationsService {
   private baseUrl = 'http://localhost:8080';
   url: string = this.baseUrl + "/api/socket";
   restUrl: string = this.baseUrl + "/sendMessageRest";
+  
+  private stompClient: Stomp.Client | undefined;
 
   constructor(private http: HttpClient) { }
+
+  sendMessageUsingSocket(notification: Notification) {
+    this.stompClient!.send(this.baseUrl+"/socket-subscriber/send/message", {}, JSON.stringify(notification));
+  }
 
   post(data: Notification) {
     return this.http.post<Notification>(this.url, data)
@@ -36,6 +45,10 @@ export class NotificationsService {
 
   getNotificationsByUserId(id: number): Observable<Notification[]> {
     return this.http.get<Notification[]>(`${this.apiUrl}/user/${id}`);
+  }
+
+  getEnabledNotificationsByUserId(id: number): Observable<Notification[]> {
+    return this.http.get<Notification[]>(`${this.apiUrl}/user/enabled/${id}`);
   }
 
   createNotification(notification: Notification): Observable<Notification> {
