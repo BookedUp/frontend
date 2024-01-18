9d06aa8a34704bc8b9bfda2e0adab7de243dabf4
0101dusica <trbovicdusica@gmail.com>
2024-01-18 15:44:12 +0100

    updated functions to connect and send messages

--- bookedUp/src/app/shared/notifications/service/web-socket.service.ts
+++ bookedUp/src/app/shared/notifications/service/web-socket.service.ts
@@ -2,33 +2,65 @@ import { Injectable } from '@angular/core';
 import { Notification } from '../model/notification.model';
 
 import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
+import { AuthService } from 'src/app/infrastructure/auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebSocketService {
-
-  private baseUrl = 'http://localhost:8080';
-  url: string = this.baseUrl + "/api/socket";
-  restUrl: string = this.baseUrl + "/sendMessageRest";
   
   private stompClient: Stomp.Client | undefined;
+  hasWebSocketNotification: Observable<boolean> = of(false);
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+  private authService: AuthService) {
+  }
+
+  connectToWebSocket(): void {
+    const socket = new SockJS('http://localhost:8080/socket');
+    this.stompClient = Stomp.over(socket);
+
+    this.stompClient.connect({}, (frame) => {
+      console.log('Connected: ' + frame);
+      if (this.stompClient !== undefined) {
+        this.stompClient.subscribe('/topic/messages', (message) => {
+          this.handleWebSocketMessage(message);
+        });
+      }
+    }, (error) => {
+      console.log('Error: ' + error);
+    });
+  }
+
+  private handleWebSocketMessage(message: Stomp.Message): void {
+    console.log('Received message: ' + message.body);
+    if (Number(message.body) === this.authService.getUserID()) {
+      this.hasWebSocketNotification = of(true);
+    }
+  }
 
   sendMessageUsingSocket(notification: Notification) {
-    this.stompClient!.send(this.baseUrl+"/socket-subscriber/send/message", {}, JSON.stringify(notification));
+    const socket = new SockJS('http://localhost:8080/socket');
+    this.stompClient = Stomp.over(socket);
+  
+    this.stompClient.connect({}, (frame) => {
+      const userId = 2;
+      if(userId != undefined && this.stompClient !== undefined){
+        this.stompClient.send('/app/send/message', {}, userId.toString());
+      }
+    }, (error) => {
+      console.log('Error: ' + error);
+    });
   }
 
-  post(data: Notification) {
-    return this.http.post<Notification>(this.url, data)
-      .pipe(map((data: Notification) => { return data; }));
-  }
-
-  postRest(data: Notification) {
-    return this.http.post<Notification>(this.restUrl, data)
-      .pipe(map((data: Notification) => { return data; }));
+  disconnectFromWebSocket(): void {
+    if (this.stompClient) {
+      this.stompClient.disconnect(() => {
+        console.log('Disconnected');
+      });
+    }
   }
 }
